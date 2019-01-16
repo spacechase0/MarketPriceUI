dd1042dada82734c8edef52bafbf0d814814e413
spacechase0 <spacechase0.and.cat@gmail.com>
2019-01-16 17:13:42 -0500

    Initial commit

---
+++ MarketPriceDisplay.cs
@@ -0,0 +1,21 @@
+﻿using Pathea.ModuleNs;
+using Pathea.StoreNs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace MarketPriceUI
+{
+    public class MarketPriceDisplay : MonoBehaviour
+    {
+        public void OnGUI()
+        {
+            var str = TextMgr.GetStr(250011); // Market Price:{0}
+            str = string.Format(str, string.Format("{0:P0}", Module<StoreManagerV40>.Self.CurPriceIndex));
+
+            GUI.Label(new Rect(5, 5, 150, 50), str);
+        }
+    }
+}
---
+++ Mod.cs
@@ -0,0 +1,32 @@
+﻿using Ccc;
+using Harmony;
+using Pathea;
+using Pathea.ModuleNs;
+using Pathea.StoreNs;
+using Pathea.UISystemNs;
+using Pathea.UISystemNs.PlayUI;
+using spacechase0.MiniModLoader.Api;
+using spacechase0.MiniModLoader.Api.Events;
+using spacechase0.MiniModLoader.Api.Mods;
+using System;
+using System.Reflection;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace MarketPriceUI
+{
+    public class Mod : IMod
+    {
+        public override void AfterModsLoaded()
+        {
+            Events.PlayerLeavesBed += playerLeftBed;
+        }
+
+        private static void playerLeftBed(object sender, EventArgs args)
+        {
+            var obj = GameObject.Find("/Canvas");
+            obj.AddComponent<MarketPriceDisplay>();
+        }
+    }
+}
